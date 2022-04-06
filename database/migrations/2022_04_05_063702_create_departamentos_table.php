b788b30a7749a578f102cf1b66acb7fa6bcba1d4
Gael Gonzales <gaelinho76@gmail.com>
2022-04-06 11:59:07 -0500

    cambio name a nombre en la migracion

--- database/migrations/2022_04_05_063702_create_departamentos_table.php
+++ database/migrations/2022_04_05_063702_create_departamentos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('departamentos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
--- database/migrations/2022_04_06_025451_create_metodo_pagos_table.php
+++ database/migrations/2022_04_06_025451_create_metodo_pagos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('metodo_pagos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
