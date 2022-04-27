0f1b796a14050c47feb1ca4f877d7c1c13c14a6f
mbsa96 <mbsa96@gmail.com>
2022-04-26 20:56:16 -0500

    Create index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div>
+        <!-- Div imagenes  Piero -->
+    </div>
+
+    <div>
+        <!-- Caracteristicas del servicio  Romano -->
+    </div>
+
+    <div>
+        <!-- Los mas vendidos  Marcos -->
+    </div>
+
+    <div>
+        <!-- Comprar por categorias  Gael -->
+    </div>
+    
+</div>
+@endsection
