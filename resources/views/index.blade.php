887e8af2ac18f24ff1bed85455816ee472b6141c
mbsa96 <mbsa96@gmail.com>
2022-04-27 00:53:43 -0500

    Create mas vendido

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    
+
     <div>
         <!-- Div imagenes  Piero -->
     </div>
@@ -13,11 +13,38 @@
 
     <div>
         <!-- Los mas vendidos  Marcos -->
+        <div class="p-4 bg-white text-center">
+            <h1>Lo mas vendido</h1>
+            <div class="d-flex justify-content-between">
+                @foreach($mostSelled as $product)
+                <div class="card" style="width: 18rem;">
+                    <a href="#">
+                        <img class="card-img-top cover" src="{{ asset('storage').'/'.$product->image }}" alt="Card image cap" height="200px">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">{{$product->name}}</h5>
+                        <p class="card-text">{{$product->description}}</p>
+                        <h3 class="text-primary">S/. {{$product->price}}</h3>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <button class="btn btn-primary"> Ver todo </button>
+        </div>
     </div>
 
     <div>
         <!-- Comprar por categorias  Gael -->
     </div>
-    
 </div>
 @endsection
+
+<style>
+    .cover {
+        object-fit: cover;
+    }
+
+    /* .card {
+        border: none !important;
+    } */
+</style>
