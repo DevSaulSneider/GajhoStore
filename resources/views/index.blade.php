e611f1479757bec7ffb50faf0a3f8f4c57c23b42
Gael Gonzales <gaelinho76@gmail.com>
2022-04-27 08:03:23 -0500

    index de category

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,61 @@
 
     <div>
         <!-- Comprar por categorias  Gael -->
+        <div class="fila">
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/laptop.jpg')}}"></div></div>
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/celular.jpg')}}"></div></div>
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/drone.jpg')}}"></div></div>
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/tablet.jpg')}}"></div></div>
+        </div>
+        <div class="fila">
+            <div class="columna"><label for=""><b>Computers</b></label></div>
+            <div class="columna"><label for=""><b>Mobile</b></label></div>
+            <div class="columna"><label for=""><b>Drones & Cameras</b></label></div>
+            <div class="columna"><label for=""><b>Tablets</b></label></div>
+        </div> <br><br>
+        <div class="fila">
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/televisor.jpg')}}"></div></div>
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/reloj.jpg')}}"></div></div>
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/parlante.jpg')}}"></div></div>
+            <div class="columna"><div class="circular--landscape"><img src="{{URL::asset('image/img-category/audifonos.jpg')}}"></div></div>
+        </div>
+        <div class="fila">
+            <div class="columna"><label for=""><b>T.V & Home Cinema</b></label></div>
+            <div class="columna"><label for=""><b>Werable Tech</b></label></div>
+            <div class="columna"><label for=""><b>Speakers</b></label></div>
+            <div class="columna"><label for=""><b>Headphones</b></label></div>
+        </div>
     </div>
+ 
     
 </div>
+
+<style>
+     .columna {
+            float: left;
+            width: 25%;
+            padding-right: 5px;
+        }
+
+        .fila::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
+    .circular--landscape {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  border-radius: 50%;
+  background-color: black;
+}
+
+.circular--landscape img {
+  width: 100%;
+  height: auto;
+}
+
+
+</style>
 @endsection
