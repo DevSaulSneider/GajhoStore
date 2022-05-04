98b3ffbf07a09bfa6f2ff8498246b0d2b9250c65
mbsa96 <mbsa96@gmail.com>
2022-05-04 10:55:29 -0500

    Fixed view employees and and validations to category

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,14 +1,31 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
+<section class="content container">
+    <div class="">
+        <div class="col-md-12">
 
-<form action="{{ url('/employee/'.$employee->id) }}" method="post">
-    @csrf
-    @include('employee.form',['mode'=>'Editar'])
-    {{ method_field('PATCH') }}
-</form>
+            @includeif('partials.errors')
 
-</div>
+            <div class="card card-default">
+                <div class="card-header">
+                    <span class="card-title">Editar Empleado</span>
+                    
+                </div>
+                        
+                <div class="card-body">
+                    <form method="POST" action="{{ route('employee.update', $employee->id) }}" role="form">
+                        {{ method_field('PATCH') }}
+                        @csrf
+
+                        @include('employee.form',['mode'=>'Editar'])
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
 
 @endsection
